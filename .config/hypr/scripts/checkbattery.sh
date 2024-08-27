74e29278efa95959d1c6ad829c08548ecb4786f8
stepskop <skopek.stepan@gmail.com>
2024-08-27 21:51:53 +0200

    config update

--- .config/hypr/scripts/checkbattery.sh
+++ .config/hypr/scripts/checkbattery.sh
@@ -7,10 +7,10 @@ ac_status=0
 last_ac_status=0
 
 while true; do
-	ac_status=$(cat /sys/class/power_supply/AC0/online)
-	if [ $ac_status -eq 1 ]; then
-		final_dialog=0
-	fi
+    ac_status=$(cat /sys/class/power_supply/AC0/online)
+    if [ $ac_status -eq 1 ]; then
+	final_dialog=0
+    fi
     bat_lvl=$(cat /sys/class/power_supply/${BAT}/capacity)
     if [ $bat_lvl -gt $last_notify ]; then
             last_notify=$bat_lvl
@@ -24,12 +24,12 @@ while true; do
         fi
     done
     if [ $bat_lvl -le 5 ]; then
-	    if [ $final_dialog -eq 0 ]; then
-			if [ $ac_status -eq 0 ]; then
-		    	final_dialog=1
-		    	zenity --warning --text="Battery is at $bat_lvl%, get ready to say goodbye" --icon=battery-low
-			fi
-	    fi
+     	if [ $final_dialog -eq 0 ]; then
+      		if [ $ac_status -eq 0 ]; then
+	 		final_dialog=1
+	 		zenity --warning --text="Battery is at $bat_lvl%, get ready to say goodbye" --icon=battery-low			
+      		fi
+     	fi
     fi
 sleep 30
 done
